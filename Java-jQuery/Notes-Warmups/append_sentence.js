46d75046b73d1c261eabd32407874454a6334998
Jamie <jamie.sam2006@gmail.com>
2017-03-21 23:33:48 -0700

    append_sentence.js

---
+++ Java-jQuery/Notes-Warmups/append_sentence.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Jamie on 3/21/2017.
+ */
+
+
+$(document).ready(function(){
+    $('#button').on("click", function(){
+        $('#sentenceOne').append(" how are you?");
+    });
+
+});
