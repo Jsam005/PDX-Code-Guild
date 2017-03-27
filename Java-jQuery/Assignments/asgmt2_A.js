068b75976b6589f1765365feff581b7596f7c24b
Jamie <jamie.sam2006@gmail.com>
2017-03-26 23:21:42 -0700

    asgmt2_A.js

---
+++ Java-jQuery/Assignments/asgmt2_A.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Jamie on 3/20/2017.
+ */
+
+$(document).ready(function(){
+    $('#button').on("click", function(){
+
+            $('.container').append('<div class="square"></div>');
+    });
+});
